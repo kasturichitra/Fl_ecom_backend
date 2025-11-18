dae1e961dc35352e094fa73f6d27ac80ecf7c880
Sandeep Kumar Lingamguntla <SandeepKumar@ntar.local>
2025-11-18 09:59:41 +0530

    Fix: Naming conventions in Banner Module from model to routes

--- Banners/bannerModel.js
+++ Banners/bannerModel.js
@@ -1,53 +1,52 @@
 import mongoose from "mongoose";
 import { getTenanteDB } from "../Config/tenantDB.js";
 
-const offerBannerSchema = new mongoose.Schema(
-    {
-        banner_title: {
-            type: String,
-            required: [true, "Banner title is required"],
-            trim: true,
-        },
-        banner_subtitle: {
-            type: String,
-            trim: true,
-        },
-        banner_description: {
-            type: String,
-            trim: true,
-        },
-        banner_image: {
-            type: String,
-            required: [true, "Banner image is required"],
-        },
-        banner_link: {
-            type: String,
-            trim: true,
-        },
-        start_date: {
-            type: Date,
-            required: [true, "Start date is required"],
-        },
-        end_date: {
-            type: Date,
-            required: [true, "End date is required"],
-        },
-        is_active: {
-            type: Boolean,
-            default: true,
-        },
-        priority: {
-            type: Number,
-            default: 1,
-        },
+const BannerSchema = new mongoose.Schema(
+  {
+    banner_title: {
+      type: String,
+      required: [true, "Banner title is required"],
+      trim: true,
     },
-    { timestamps: true }
+    banner_subtitle: {
+      type: String,
+      trim: true,
+    },
+    banner_description: {
+      type: String,
+      trim: true,
+    },
+    banner_image: {
+      type: String,
+      required: [true, "Banner image is required"],
+    },
+    banner_link: {
+      type: String,
+      trim: true,
+    },
+    start_date: {
+      type: Date,
+      required: [true, "Start date is required"],
+    },
+    end_date: {
+      type: Date,
+      required: [true, "End date is required"],
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+    priority: {
+      type: Number,
+      default: 1,
+    },
+  },
+  { timestamps: true }
 );
 
-const OfferBannerModel = async (tenantId) => {
-    const db = await getTenanteDB(tenantId);
-    return db.models.OfferBanner || db.model("OfferBanner", offerBannerSchema);
+const BannerModel = async (tenantId) => {
+  const db = await getTenanteDB(tenantId);
+  return db.models.Banner || db.model("Banner", BannerSchema);
 };
 
-
-export default OfferBannerModel
+export default BannerModel;
--- Banners/bannersService.js
+++ Banners/bannersService.js
@@ -1,96 +1,86 @@
-import { log } from "console";
-import OfferBannerModel from "./bannerModel.js";
+import BannerModel from "./bannerModel.js";
 import fs from "fs";
 import path from "path";
 
-export const createBannersServices = async (tenantID, bannerData) => {
-    try {
-        if (!tenantID) throw new Error("Tenant ID is required");
+export const createBannerService = async (tenantID, bannerData) => {
+  try {
+    if (!tenantID) throw new Error("Tenant ID is required");
 
-        const bannerModelDB = await OfferBannerModel(tenantID);
-        const createdBanner = await bannerModelDB.create(bannerData);
+    const bannerModelDB = await BannerModel(tenantID);
+    const createdBanner = await bannerModelDB.create(bannerData);
 
-        return createdBanner;
-    } catch (error) {
-        throw new Error(error.message || "Error creating offer banner");
-    }
+    return createdBanner;
+  } catch (error) {
+    throw new Error(error.message || "Error creating offer banner");
+  }
 };
 
+export const getAllBannersService = async (tenantID) => {
+  try {
+    if (!tenantID) throw new Error("Tenant ID is required");
 
+    const bannerModelDB = await BannerModel(tenantID);
+    const response = await bannerModelDB.find().sort({ createdAt: -1 });
 
-export const getAllBannersServices = async (tenantID) => {
-    try {
-        if (!tenantID) throw new Error("Tenant ID is required");
-
-        const bannerModelDB = await OfferBannerModel(tenantID);
-        const response = await bannerModelDB.find().sort({ createdAt: -1 });
-
-        return response;
-    } catch (error) {
-        throw new Error(error.message || "Error fetching offer banners");
-    }
+    return response;
+  } catch (error) {
+    throw new Error(error.message || "Error fetching offer banners");
+  }
 };
 
-
-
-export const updatebannersServices = async (tenantID, bannerId, updatedData, newImageFilename) => {
-    try {
-        if (!tenantID) throw new Error("Tenant ID is required");
-        if (!bannerId) throw new Error("Banner ID is required");
-
-        const bannerModelDB = await OfferBannerModel(tenantID);
-        const existingBanner = await bannerModelDB.findById(bannerId);
-
-        if (!existingBanner) throw new Error("Banner not found");
-
-        const deleteFileIfExists = (filePath) => {
-            if (!filePath) return;
-
-            let cleanedPath = filePath
-                .replace(/\\/g, "/")
-                .replace(/^\/+/, "");
-
-            const fullPath = path.resolve(process.cwd(), cleanedPath);
-
-            try {
-                if (fs.existsSync(fullPath)) {
-                    fs.unlinkSync(fullPath);
-                    console.log("Deleted:", fullPath);
-                } else {
-                    console.warn("File not found:", fullPath);
-                }
-            } catch (err) {
-                console.warn("Failed to delete:", fullPath, err.message);
-            }
-        };
-
-        if (newImageFilename && existingBanner.banner_image) {
-
-            const oldImagePath = existingBanner.banner_image;
-
-            deleteFileIfExists(oldImagePath);
-            updatedData.banner_image = newImageFilename;
-        }
-
-        const updatedBanner = await bannerModelDB.findByIdAndUpdate(bannerId, updatedData, {
-            new: true,
-            runValidators: true,
-        });
-
-        return updatedBanner;
-    } catch (error) {
-        throw new Error(error.message || "Error updating banner");
-    }
-};
-
-
-
-export const deleteBannerServices = async (tenantID, bannerId) => {
+export const updateBannerService = async (tenantID, bannerId, updatedData, newImageFilename) => {
   try {
     if (!tenantID) throw new Error("Tenant ID is required");
     if (!bannerId) throw new Error("Banner ID is required");
 
-    const bannerModelDB = await OfferBannerModel(tenantID);
+    const bannerModelDB = await BannerModel(tenantID);
+    const existingBanner = await bannerModelDB.findById(bannerId);
+
+    if (!existingBanner) throw new Error("Banner not found");
+
+    const deleteFileIfExists = (filePath) => {
+      if (!filePath) return;
+
+      let cleanedPath = filePath.replace(/\\/g, "/").replace(/^\/+/, "");
+
+      const fullPath = path.resolve(process.cwd(), cleanedPath);
+
+      try {
+        if (fs.existsSync(fullPath)) {
+          fs.unlinkSync(fullPath);
+          console.log("Deleted:", fullPath);
+        } else {
+          console.warn("File not found:", fullPath);
+        }
+      } catch (err) {
+        console.warn("Failed to delete:", fullPath, err.message);
+      }
+    };
+
+    if (newImageFilename && existingBanner.banner_image) {
+      const oldImagePath = existingBanner.banner_image;
+
+      deleteFileIfExists(oldImagePath);
+      updatedData.banner_image = newImageFilename;
+    }
+
+    const updatedBanner = await bannerModelDB.findByIdAndUpdate(bannerId, updatedData, {
+      new: true,
+      runValidators: true,
+    });
+
+    return updatedBanner;
+  } catch (error) {
+    throw new Error(error.message || "Error updating banner");
+  }
+};
+
+export const deleteBannerService = async (tenantID, bannerId) => {
+  try {
+    if (!tenantID) throw new Error("Tenant ID is required");
+    if (!bannerId) throw new Error("Banner ID is required");
+
+    const bannerModelDB = await BannerModel(tenantID);
     console.log(bannerId, "bannerId");
 
     const existingBanner = await bannerModelDB.findById(bannerId);
@@ -113,7 +103,7 @@ export const deleteBannerServices = async (tenantID, bannerId) => {
     };
 
     if (existingBanner.banner_image) {
-      const oldImagePath = existingBanner.banner_image
+      const oldImagePath = existingBanner.banner_image;
       deleteIfExists(oldImagePath);
     }
 
