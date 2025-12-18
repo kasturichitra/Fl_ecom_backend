ed69ce39a019106b1dba6897f62e4d32485fd7e9
rajeshtanne <rajeshtanne@ntar.com>
2025-12-18 18:33:42 +0530

    update : contact info service file get function

--- ContactInfo/contactInfoService.js
+++ ContactInfo/contactInfoService.js
@@ -7,27 +7,27 @@ import throwIfTrue from "../utils/throwIfTrue.js";
 export const getContactInfoService = async (tenantId) => {
     throwIfTrue(!tenantId, "Tenant ID is required.");
     const ContactInfoDB = await ContactInfoModel(tenantId);
-    return await ContactInfoDB.find().sort({ createdAt: -1 });
+    return await ContactInfoDB.findOne();
 };
 
 export const updateContactInfoService = async (tenantId, updateData) => {
-  throwIfTrue(!tenantId, "Tenant ID is required.");
-  const ContactInfoDB = await ContactInfoModel(tenantId);
+    throwIfTrue(!tenantId, "Tenant ID is required.");
+    const ContactInfoDB = await ContactInfoModel(tenantId);
 
-  const existingInfo = await ContactInfoDB.findOne();
+    const existingInfo = await ContactInfoDB.findOne();
 
-  if (existingInfo?.logo_image && updateData.logo_image &&
-      existingInfo.logo_image !== updateData.logo_image) {
+    if (existingInfo?.logo_image && updateData.logo_image &&
+        existingInfo.logo_image !== updateData.logo_image) {
 
-    const oldImagePath = path.resolve(existingInfo.logo_image);
-    fs.existsSync(oldImagePath) && fs.unlinkSync(oldImagePath);
-  }
+        const oldImagePath = path.resolve(existingInfo.logo_image);
+        fs.existsSync(oldImagePath) && fs.unlinkSync(oldImagePath);
+    }
 
-  return await ContactInfoDB.findOneAndUpdate(
-    {},
-    updateData,
-    { new: true, upsert: true } // 🔥 auto create or update
-  );
+    return await ContactInfoDB.findOneAndUpdate(
+        {},
+        updateData,
+        { new: true, upsert: true } // 🔥 auto create or update
+    );
 };
 
 
