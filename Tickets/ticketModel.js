d921c23da30ece349e5fed6436e63bba5a9379f7
rajeshtanne <rajeshtanne@ntar.com>
2025-12-22 19:20:25 +0530

    Test

--- Tickets/ticketModel.js
+++ Tickets/ticketModel.js
@@ -28,6 +28,7 @@ const chatMessageSchema = new mongoose.Schema(
   },
   { _id: false }
 );
+//Tst commit to check the git 
 
 // Pure chat room (no ticket fields)
 const chatSchema = new mongoose.Schema(
