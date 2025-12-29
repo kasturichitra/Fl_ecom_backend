28fdb681a6e1e68dfb1705dac0c0964f0a1bcb46
Bhanuprasad Suram <bhanuprasadsuram@ntar.com>
2025-12-29 13:22:09 +0530

    created getInTouch api

--- ContactInfo/contactInfoService.js
+++ ContactInfo/contactInfoService.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import throwIfTrue from "../utils/throwIfTrue.js";
 import { getTenantModels } from "../lib/tenantModelsCache.js";
+import { sendEmail } from "../utils/sendOTP.js";
 
 export const getContactInfoService = async (tenantId) => {
   throwIfTrue(!tenantId, "Tenant ID is required.");
@@ -26,3 +27,21 @@ export const updateContactInfoService = async (tenantId, updateData) => {
     { new: true, upsert: true } // 🔥 auto create or update
   );
 };
+
+
+
+export const getInTouchService = async (tenantID, data) => {
+
+  // console.log(data,"data")
+
+  const { email } = data
+
+  const message = "welcome to Ntar"
+  const subject = "Hi bro nice to meet you"
+
+  throwIfTrue(!email, "email required")
+
+  const result = await sendEmail(email, message, subject)
+  
+  return result
+}
