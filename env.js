52cdc0958a630459bf2904fb600cc8ed31762e55
Sandeep Kumar Lingamguntla <SandeepKumar@ntar.local>
2026-01-23 16:46:17 +0530

    Setup Bull MQ

--- env.js
+++ env.js
@@ -1,13 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-
 
 export const mongoUri = process.env.MONGODB_URL;
 export const port_number = process.env.PORT;
-export const jwtSecret=process.env.JWT_SECRET
+export const jwtSecret = process.env.JWT_SECRET;
 
-export const smtpUser=process.env.SMTP_USER
-export const smtpPass=process.env.SMTP_PASS
+export const redisHost = process.env.REDIS_HOST;
+export const redisPort = process.env.REDIS_PORT;
+export const redisPassword = process.env.REDIS_PASSWORD;
 
-export const couponAlphabet=process.env.COUPON_ALPHABET
-export const gstinVerifyUrl=process.env.GSTIN_VERIFY_URL
+export const smtpUser = process.env.SMTP_USER;
+export const smtpPass = process.env.SMTP_PASS;
+
+export const couponAlphabet = process.env.COUPON_ALPHABET;
+export const gstinVerifyUrl = process.env.GSTIN_VERIFY_URL;
